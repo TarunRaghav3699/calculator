e7053fc91f1114dbca0ea5a03c300d0f0ed4e8b1
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-09-05 10:14:57 +0000

    updated code removed Invalid Input

--- Calculator.py
+++ Calculator.py
@@ -21,7 +21,7 @@ while True:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
         except ValueError:
-            print("Invalid input.Please enter a number.")
+            print("Please enter a number.")
             continue
         if choice =='1':
             print(num1,"+",num2,"=",add(num1, num2))
@@ -34,6 +34,5 @@ while True:
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
             break
-        else:
-            print("Invalid Input")
+       
         
